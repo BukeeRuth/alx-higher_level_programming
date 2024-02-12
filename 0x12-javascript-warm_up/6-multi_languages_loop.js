bae2ad1e76324744ee73403fbbd3987ac40006dd
BukeeRuth <olatunjibukola018@gmail.com>
2024-02-12 14:41:52 -0800

    multi lang

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-while (i < languages.length) {
-	console.log(languages.[i]);
-	i++;
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	console.log(line);
 }
+
