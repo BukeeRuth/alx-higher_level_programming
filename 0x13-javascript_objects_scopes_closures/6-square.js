e6a23a51b6d510de215f71de12591f6305fe89c6
BukeeRuth <olatunjibukola018@gmail.com>
2024-02-14 18:26:26 -0800

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Square = require('./6-square');
+const Square1 = require('./5-square');
 
-class Square extends Square {
+class Square extends Square1 {
   charPrint(c) {
     if (c === undefined) {
       c = 'X';
