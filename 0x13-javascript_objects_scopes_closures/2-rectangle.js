dd0d7ba3e33bd38419fccb4c04381ce062a2439d
BukeeRuth <olatunjibukola018@gmail.com>
2024-02-14 17:03:12 -0800

    rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-        this.width = w;
-        this.height = h;
-
       if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
         return {};
       }
+      this.width = w;
+      this.height = h;
     }
 }
 module.exports = Rectangle;
